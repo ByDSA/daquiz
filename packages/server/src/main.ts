dbc9af90b8d78defa40f86a035cb5b3929c150f9
Daniel Sales <danisales.es@gmail.com>
2024-05-24 16:26:53 +0200

    feat(back): enable CORS for development environment

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,6 +6,19 @@ import { AddStatusCodeInterceptor } from "./utils/interceptors/AddStatusCodeInte
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors( {
+      origin: (origin, callback) => {
+        if (!origin || origin.startsWith("http://localhost")) // Cualquier puerto
+          callback(null, true);
+        else
+          callback(new Error("Not allowed by CORS"));
+      },
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+      credentials: true,
+    } );
+  }
+
   app.useGlobalPipes(new ValidationPipe( {
     transform: true,
   } ));
